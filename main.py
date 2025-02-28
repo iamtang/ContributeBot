b5931c8451bcf3b6dcf36a76d0e6e9e96d62bdfd
JJ <tangjun@zuzuche.com>
2025-02-28 15:32:50 +0800

    add

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from GITHUB import GITHUB
 import random
 
 load_dotenv()
+range = os.getenv("RANGE")
 github = GITHUB(
     GITHUB_TOKEN = os.getenv("GITHUB_TOKEN"),
 	USERNAME = os.getenv("USERNAME"),
@@ -16,13 +17,13 @@ def main():
     # github.delCommentIssue(commentData['id'])
     # github.closeIssue(issueData['number'])
     # res = github.updateFile('README.md')
-    random_number = random.randint(0, 6)
+    _range = list(map(int, range.split('-')))
+    random_number = random.randint(*_range)
     i = 0
     while i < random_number:
         i+=1
         github.updateFile('README.md')
 
-    print(random_number)
     
 if __name__ == '__main__':
     main()
